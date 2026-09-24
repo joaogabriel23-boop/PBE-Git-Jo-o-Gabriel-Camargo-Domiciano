c6a4d7c79b763563d36d4066039e4c74de836ea9
joaogabriel23-boop <joaocamargo0654@gmail.com>
2026-09-24 14:01:30 -0300

    g

---
+++ Index.php
@@ -0,0 +1,13 @@
+<?php
+    echo "<br>";
+    $alunos = array("João", "Maria", "Pedro", "Ana", "Lucas");
+    foreach ($alunos as $aluno) {
+     echo $aluno . "<br>";
+    }
+
+    echo "<br>";
+    $Turmas = array("2°ano A", "5°Ano B", "7°ano B", "1°Ano EM B", "2°ano EM A", "5°Ano A");
+    foreach ($Turmas as $Turma) {
+     echo $Turma . "<br>";
+    }
+?>
---
+++ reame.md
@@ -0,0 +1,3 @@
+# meu primeiro codigo php com git
+
+meu primeiro codigo de php com imprimindo uma lista de nome e turmas
