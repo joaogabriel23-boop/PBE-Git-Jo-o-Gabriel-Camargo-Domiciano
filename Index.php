f8242e47d43eb73c736690dead779ce1e3096aac
joaogabriel23-boop <joaocamargo0654@gmail.com>
2026-09-24 14:38:10 -0300

    Index.php V3.0

--- Index.php
+++ Index.php
@@ -1,7 +1,10 @@
 <?php
-    echo "<br>";
-    $alunos = array("João  2°ano A", "Maria  5°Ano B", "Pedro  7°ano B", "Ana  1°Ano EM B", "Lucas  2°ano EM A");
-    foreach ($alunos as $aluno) {
-     echo $aluno . "<br>";
-    }
-?>
+$nome = "joão";
+$idade = "17 Anos";
+$curso = "Desenvolvimento de Sistemas";
+$turma = "1 ano ° A DS";
+
+echo "Aluno é $nome";
+echo "Tem $idade Anos";
+echo "Cursa $curso";
+echo "Na turma $turma";
